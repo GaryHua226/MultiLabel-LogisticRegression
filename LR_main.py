41a91b9c4fb9c36656db35025fdd9e676a116341
jkq <1154609706@qq.com>
2020-10-20 09:23:36 +0800

    增加了打印函数

--- LR_main.py
+++ LR_main.py
@@ -15,7 +15,7 @@ def get_test_data():
 def sigmoid(z):
     return 1/(1+np.exp(-z))
 
-def get_accuracy(predicted_y):
+def get_accuracy(predicted_y, test_y):
     correct = 0
     for i in range(len(predicted_y)):
         if predicted_y[i]==test_y[i]:
@@ -50,11 +50,44 @@ def get_F1(precision, recall):
     F1=2*precision*recall/(precision+recall)
     return F1
 
+def print_performance(accuracy, microPrecision, microRecall, microF1,macroPrecision,macroRecall,macroF1):
+    print('accuracy: ',accuracy)
+    print('micro Precision: ', microPrecision)
+    print('micro Recall: ',microRecall)
+    print('micro F1: ', microF1)
+    print('macro Precision: ', macroPrecision)
+    print('macro Recall: ',macroRecall)
+    print('macro F1: ', macroF1)
+
 
 if __name__ == '__main__':
     train_X, train_y=get_train_data()
     test_X, test_y=get_test_data()
 
+    CLASSNUM = 26 # 类别数量
+
+    TPList=[]
+    FPList=[]
+    FNList=[]
+    FPList=[]
+
+    for i in range(CLASSNUM):
+        pass
+        TPList.append(TP)
+        FPList.append(FP)
+        TNList.append(TN)
+        FNList.append(FN)
+
 
     # micro是用总的TP,FP,TN,FN求指标
     # macro是对各个分类器的指标求平均
+
+    accuracy=get_accuracy(predicted_y, test_y)
+    microPrecision=get_microPrecision(TPList, FPList)
+    microRecall=get_microRecall(TPList, FNList)
+    microF1=get_microF1(microPrecision, microRecall)
+    macroPrecision = mean(precisionList)
+    macroRecall = mean(recallList)
+    macroF1 = mean(F1List)
+
+    print_performance(accuracy, microPrecision, microRecall, microF1,macroPrecision,macroRecall,macroF1)
